ecbca91b2edb8ba450859a26c9e2342ce3c57773
mNuraiym <nuraika180903@gmail.com>
2023-02-22 16:14:58 +0600

    dz3

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: 'Geeks',
+      age: 15
+    };
+    this.handleChange = this.handleChange.bind(this);
+    
+  }
+  handleChange() {
+    this.setState({
+      name: 'Geeksss',
+      age:19
+    }
+      
+    );
+  }
+  render() {
+    return (
+      <div className="App">
+       <button onClick ={this.handleChange}>нажми на меня</button>
+       <p>name:  {this.state.name}</p>
+       <p>age: {this.state.age}</p>
+      </div>
+    );
+  
+}
+  
 }
 
 export default App;
